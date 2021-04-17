34e5eb86c6e7b955e6db822602423b30fb5a15db
AbidHanif <qadriturabi12@gmail.com>
2021-04-18 03:45:23 +0500

    4 question completed

---
+++ app.js
@@ -0,0 +1,47 @@
+// ******Question 1*********
+// var cityname = "Karachi"
+// var anoterCity = "Islamabad"
+// var answer = prompt("enter city")
+// if(answer === "Karachi"){
+//     alert ("Welcome To The City Of Light")
+// }
+// if(answer === "Islamabad"){
+//     alert ("Welcome To Capital Of Pakistan")
+// }
+
+// ******Question 2***********
+// var gender = "Male"
+// var anotherGender = "Female"
+// var answer = prompt("Your Gender")
+// if(answer === "Male"){
+//     alert ("Good Morning Sir")
+// }
+// if(answer === "Female"){
+//     alert ("Good Morning Ma'am")
+// }
+
+// Question 3
+// var colorOne = "red"
+// var colorTwo = "green"
+// var colorThree = "yellow"
+// var answer = prompt("signal color")
+// if(answer === "red"){
+//     alert ("you must stop")
+// }
+// if(answer === "green"){
+//     alert ("you may go now")
+// }
+// if(answer === "yellow"){
+//     alert ("ready to move")
+// }
+
+// Quetion no 4
+// var petrol = "0.25liter"
+// var petrolNew = "1 liter"
+// var answer = prompt("enter remaining petrol")
+// if(answer < "0.25liter"){
+//     alert ("Please Refill the fuel tank")
+// }
+// if(answer > "0.25liter"){
+//     alert ("dont worry keep moving")
+// }
