3d3cfa773298be4dcb7b3729a2d1546cf4c26498
AbidHanif <qadriturabi12@gmail.com>
2021-04-19 03:33:05 +0500

    question 5,6,7,10 completed

--- app.js
+++ app.js
@@ -45,3 +45,89 @@
 // if(answer > "0.25liter"){
 //     alert ("dont worry keep moving")
 // }
+
+// Question no 5
+// var a = 4
+// if(++a===5){
+//     alert("given condition for variable is a true")
+// }
+
+// var b = 82
+// if (b++ === 83){
+// alert("given condition for variable b is true")
+// }
+
+// var c = 12
+// if(c++ === 13){
+//     alert("condition 1 is true")
+// }
+// if(c===13){
+//     alert("condition 2 is true")
+// }
+// if (++c < 14){
+//     alert("condition 3 is true");
+//     }
+// if(c === 14){
+//     alert("condition 4 is true");
+//     }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals");
+// }
+// if (true){
+//     alert("True");
+// }
+// if (false){
+//     alert("False");
+// }
+    
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+// Question No 6
+// var Aone = 80
+// var b = 70
+// var c = 60
+// var percentage = prompt("enter your percentage")
+// if (percentage>=80){
+//     alert("Excellent")
+// }
+// if (percentage>=70){
+//     alert("Good")
+// }
+// if (percentage>=60){
+//     alert("You Need To Improve")
+// }
+// if (percentage<60){
+//     alert("Fail")
+// }
+// document.write("<h1>MARK SHEET</h1> <br/> <h3>Total Marks</h3>300 <h3>Mark Obtained</h3>219 <h3>Percentage</h3>73% <h3>Grade</h3>B <h3>Remarks</h3>You Need To Improve")
+
+// Question no 7
+// var a = "6"
+// var answer = prompt("Enter Secret Number")
+// if (answer==="6"){
+//     alert("Bingo! You Are Right")
+// }
+
+// Question no 10
+// var a = 40
+// var b = 30
+// var c = 20
+// var d = 10
+// var answer = prompt("Enter Tempreture")
+// if(answer>40){
+//     alert("Its too Hot Outside")
+// }
+// if(answer>30){
+//     alert("The Wheather is Normal Today")
+// }
+// if(answer>20){
+//     alert("Weather is Cool Today")
+// }
+// if(answer>10){
+//     alert("OMG! Today's weather is so cold")
+// }
